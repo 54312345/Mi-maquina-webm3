122ec39d2d88f5c6e41efa90763cca6b506ef06f
54312345 <lg.gomezpulido@gmail.com>
2025-02-20 17:46:01 +0100

    Add files via upload
    estoy aprendiendo a como trabajar desde casa

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){let e=document.getElementById("verMasBtn");e.addEventListener("click",function(){alert("\xa1Has hecho clic en Ver m\xe1s!")})});
